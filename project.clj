e20d59f7b9c4e0f59c7bc41160e831732c11e035
Robert Krahn <robert.krahn@gmail.com>
2015-04-05 02:44:29 -0700

    0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.rksm/cloxp-source-reader "0.1.0-SNAPSHOT"
+(defproject org.rksm/cloxp-source-reader "0.1.1"
   :description "Source reading, parsing, and querying for cloxp."
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
@@ -6,5 +6,5 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.reader "0.8.16"]
                  [org.clojure/tools.analyzer.jvm "0.6.5"]
-                 [org.rksm/system-files "0.1.3-SNAPSHOT"]
+                 [org.rksm/system-files "0.1.3"]
                  [com.keminglabs/cljx "0.6.0"]])
