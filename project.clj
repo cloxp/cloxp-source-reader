a654a1f07fd63127b3e36c0e2c373254ee7ad6a7
Robert Krahn <robert.krahn@gmail.com>
2015-04-24 23:09:04 -0700

    0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.rksm/cloxp-source-reader "0.1.2"
+(defproject org.rksm/cloxp-source-reader "0.1.3"
   :description "Source reading, parsing, and querying for cloxp."
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
