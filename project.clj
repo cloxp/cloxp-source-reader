2c99d6b98c7d1d25e467aac1416aae6e374a3693
Robert Krahn <robert.krahn@gmail.com>
2015-07-21 14:18:34 -0700

    lib update

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :url "http://github.com/cloxp/cloxp-source-reader"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.reader "0.9.1"]
-                 [org.clojure/tools.namespace "0.2.8"]
-                 [org.clojure/tools.analyzer.jvm "0.6.5"]
                  [org.rksm/system-files "0.1.6"]
                  [com.keminglabs/cljx "0.6.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.reader "0.9.2"]
+                 [org.clojure/tools.namespace "0.2.10"]
+                 [org.clojure/tools.analyzer.jvm "0.6.7"]
