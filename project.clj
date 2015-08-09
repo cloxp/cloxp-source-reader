eac1de0cbb224cff35cf173f142e378c7c52506c
Robert Krahn <robert.krahn@gmail.com>
2015-08-09 18:02:36 +0200

    ensure new tools.namespace version for cljc support

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
   :url "http://github.com/cloxp/cloxp-source-reader"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.reader "0.9.2"]
-                 [org.clojure/tools.namespace "0.2.10"]
+                 [org.clojure/tools.namespace "0.2.11"]
                  [org.clojure/tools.analyzer.jvm "0.6.7"]
                  [org.rksm/system-files "0.1.7-SNAPSHOT"]])
